42ed7b9d5a5cbc909db6ef54773638fe1217b6e4
Pei Pei5 Li <lipei5@Lenovo.com>
2019-03-27 18:13:36 +0800

    handleClick

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Clock />
+        <Clock name='clock1' />
+        <Clock name='clock2' />
+        <Clock name='clock3' />
+        <Clock name='clock4'/>
       </div>
     );
   }
@@ -15,15 +18,23 @@ function formatUser(user) {
   return user.firstName + ' ' + user.lastName
 }
 
-const user = {
-  firstName: 'Jon',
-  lastName: 'Doe'
-}
+const user =
+  {
+    firstName: 'Jon1',
+    lastName: 'Doe1'
+  }
 
-class Element extends Component{
+class User extends Component{
+  constructor(props){
+    super(props)
+  }
+
+  handleClick = () => {
+    console.log('you are clicking user' + this.props.name)
+  }
   render() {
     return (
-      <h1>
+      <h1 onClick={this.handleClick}>
         hello, {formatUser(user)}!
       </h1>
     )
@@ -33,14 +44,40 @@ class Element extends Component{
 class Clock extends Component {
   constructor(props) {
     super(props);
-    this.state = {date: new Date()};
+    this.state = {
+      date: new Date(),
+      name: '',
+    };
+  }
+
+  handleClick = () => {
+    console.log('you are clicking Clock' + this.props.name)
+  }
+
+  componentDidMount () {
+    this.timerID  = setInterval(()=>{
+      this.tick()
+    },1000)
+  }
+
+  componentWillMount () {
+    clearInterval(this.timerID)
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+    this.setState({
+      name: 'userAt:' + this.state.date
+    })
   }
 
   render () {
     return (
       <div>
-        <Element/>
-        <h2>It is {this.props.date.toLocalTimeString()}.</h2>
+        <User name={this.state.name}/>
+        <h2 onClick={this.handleClick}>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     );
   }
